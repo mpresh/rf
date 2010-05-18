1ac4d65044abd58b0c27436d75e81b9c2ff40d05
Mike Preshman <mpresh@mike-preshmans-macbook-pro.local>
2010-05-18 00:22:53 -0400

    Invite to show images of friends.

--- static/myjscript.js
+++ static/myjscript.js
@@ -4,13 +4,13 @@
 function make_friend_list(data) {
     html = "";
     $.each(data,function(i,list) {
-	    html +=	friend_check_html(list["screen_name"],list["profile_image_url"],list["name"]);
+	    html +=	friend_check_html(list.name, list.profile_image_url, list.screen_name);
 	});
     return html;
 }
 
 function friend_check_html(namev,urlv,userv) {
-    html = '<input type="checkbox" name="invitee" value='+userv+'  /><img src='+urlv+'/><span>'+namev+'</span><br />';
+    html = '<input type="checkbox" name="invitee" value='+userv+'  /><img src="' + urlv + '"/><span>'+namev+'</span><br />';
     return html;
 }
 
