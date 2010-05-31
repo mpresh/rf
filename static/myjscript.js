b78018c1c4ed654821c5e190e3ff03679b00e52b
Mike Preshman <mike@mike-desktop.(none)>
2010-05-31 11:38:25 -0500

    Gettign coloring and toggling rigth on invite friends.

--- static/myjscript.js
+++ static/myjscript.js
@@ -6,12 +6,13 @@ function make_friend_list(data) {
     $.each(data,function(i,list) {
 	    html +=	friend_check_html(list.name, list.profile_image_url, list.screen_name);
 	});
-    return html;
+    return "<ul id='friend_list'>" + html + "</ul>";
 }
+
 
 function friend_check_html(namev,urlv,userv) {
-    html = '<input type="checkbox" name="invitee" value='+userv+'  /><img src="' + urlv + '"/><span>'+namev+'</span><br />';
+    html = '<li class="friend_list_item"><input type="checkbox" name="invitee" value='+userv+'  /><img src="' + urlv + '"/>&nbsp;&nbsp;&nbsp;&nbsp;<span id="friend_name_text">'+namev+'</span></li>';
+
     return html;
 }
-
 
