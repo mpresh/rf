2f8f4e466e448db9783077c30cb85f28beec949e
Mike Preshman <mpresh@mike-preshmans-macbook-pro.local>
2010-07-08 10:31:28 -0500

    Adding domain middleware.

--- common_settings.py
+++ common_settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'demo.middleware.domains.SubdomainMiddleware',
 )
 
 ROOT_URLCONF = 'demo.urls'
