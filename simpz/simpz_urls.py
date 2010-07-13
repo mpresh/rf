c0ad5c292bfbd5b0fa387d65ecc7e2398d32c070
Mike Preshman <mpresh@mike-preshmans-macbook-pro.local>
2010-07-13 11:03:40 -0500

    Fixing bug!

--- simpz/simpz_urls.py
+++ simpz/simpz_urls.py
@@ -10,8 +10,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       (r'^$', index),
-                       #url(r'^$', 'index', name='index'),
+                       url(r'^$', 'views.index', name='index'),
 
                        # twitter authentication auth module
                        url(r'info/?$', 'tauth.views.info', name='tauth_info'),
