a1fefd2781c920415f9fddba5acca96d1b10730e
Mike Preshman <mpresh@mike-preshmans-macbook-pro.local>
2010-07-13 18:03:19 -0500

    Fixing some more url stuff.

--- urls.py
+++ urls.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       (r'^simpz/',  include('simpz.simpz_urls')),
-                       (r'^admin/', include(admin.site.urls)),
-                       (r'^analytics/', include('analytics.urls')),
+                       url(r'^simpz/',  include('simpz.simpz_urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^analytics/', include('analytics.urls')),
                        url(r'^$', 'frontpage.views.frontpage', name='frontpage'),
+                       url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+                        {'document_root': settings.MEDIA_ROOT}, name="static"),
 )
