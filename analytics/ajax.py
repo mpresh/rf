cccd308b4cd2be54bd42ac8bffe82e136e4600fc
Mike Preshman <mpresh@gmail.com>
2010-07-16 00:03:37 -0500

    fixing table.

--- analytics/ajax.py
+++ analytics/ajax.py
@@ -213,11 +213,11 @@ def analytics_data(req):
     column_list.append({"id" : "reach", "label" : "Reach", "type" : "number"})
     data["cols"] = column_list
 
-
     rows_list = []
     user_dict = {}
     for share in shares:
-        name = None
+        name = ""
+        network = ""
         if share.from_account_type == "F" and share.from_user_facebook:
             fbuser = FBUser.objects.get(id=share.from_user_facebook.id)
             name = fbuser.name or fbuser.facebook_id
@@ -227,29 +227,33 @@ def analytics_data(req):
             name = user.name 
             network = "Twitter"
 
-        name_id = name + network
-
-        if name_id  not in user_dict:
+        name_id = str(name) + network
+        if name_id  not in user_dict.keys():
             user_dict[name_id] = {"name":name, 
                                   "network":network, 
                                   "num":1, 
                                   "reach":share.reach, 
                                   "totalReach":share.totalReach()}
         else:
+            print "orhere"
             user_dict[name_id]["num"] = user_dict[name_id]["num"] + 1
             user_dict[name_id]["reach"] = user_dict[name_id]["reach"] + share.reach
             user_dict[name_id]["totalReach"] = user_dict[name_id]["totalReach"] + share.totalReach()
 
     for name_id in user_dict.keys():
+        if float(user_dict[name_id]["num"]) == 0:
+            div_num = 1.0
+        else:
+            div_num = float(user_dict[name_id]["num"])
         rows_list.append({"c" : [{"v" : user_dict[name_id]["name"]}, 
                                  {"v" : user_dict[name_id]["network"]},
                                  {"v" : user_dict[name_id]["num"]},
-                                 {"v" : int(float(user_dict[name_id]["reach"]) / float(user_dict[name_id]["num"]))},
-                                 {"v" : int(float(user_dict[name_id]["totalReach"]) / float(user_dict[name_id]["num"]))}]})
+                                 {"v" : int(float(user_dict[name_id]["reach"]) / div_num)},
+                                 {"v" : int(float(user_dict[name_id]["totalReach"]) / div_num)}]})
 
     data["rows"] = rows_list
 
     dict["status"] = 200
     dict["data"] = data
-    print "Returning DICT", dict
+    print "Table Returning DICT", dict
     return HttpResponse(json.dumps(dict))
