68acf298b1a7885682e35d88b3fb69a4f0f3d49a
Mike Preshman <mpresh@mike-preshmans-macbook-pro.local>
2010-07-16 15:49:30 -0500

    Adding chart for total reach.

--- analytics/ajax.py
+++ analytics/ajax.py
@@ -9,7 +9,131 @@ from tauth.models import *
 
 from datetime import datetime, timedelta
 
-def analytics_date_range(req):
+def analytics_date_range_reach(req):
+    """Return data organized by date range."""
+    dict = {}
+    if "event" in req.GET:
+        try:
+            event = Event.objects.get(id=req.GET["event"])
+            shares = Share.objects.filter(event=event.id)
+        except Exception:
+            dict["error"] = "Valid Event id not provided."
+            dict["status"] = 500
+            return HttpResponse(json.dumps(dict))
+    else:
+        dict["error"] = "Must specify event."
+        dict["status"] = 500
+        return HttpResponse(json.dumps(dict))
+
+    range_type_list = ["minutes", "hours", "days", "weeks", "months", "years"]
+    if "range_type" in req.GET:
+        if req.GET["range_type"] not in range_type_list:
+            dict["error"] = "Valid range not provided."
+            dict["status"] = 500
+            return HttpResponse(json.dumps(dict))
+        else:
+            range_type = req.GET["range_type"]
+    elif "range_type" in req.POST:
+        if req.POST["range_type"] not in range_type_list:
+            dict["error"] = "Valid range not provided."
+            dict["status"] = 500
+            return HttpResponse(json.dumps(dict))
+        else:
+            range_type = req.POST["range_type"]
+    else:
+        dict["error"] = "range_type not specfied."
+        dict["status"] = 500
+        return HttpResponse(json.dumps(dict))
+    
+    if "range_duration" in req.GET:
+        range_duration = req.GET["range_duration"]
+    elif "range_duration" in req.POST:
+        range_duration = req.POST["range_duration"]
+    else:
+        dict["error"] = "range_duration not specfied."
+        dict["status"] = 500
+        return HttpResponse(json.dumps(dict))
+
+    try:
+        range_duration = int(range_duration)
+    except Exception:
+        dict["error"] = "range_duration must be an integer value."
+        dict["status"] = 500
+        return HttpResponse(json.dumps(dict))
+    range_duration = range_duration + 1
+    
+    data = {}
+    column_list = []
+    range_type_label = ""
+    if range_type == "minutes":
+        range_type_label = "Minutes"
+        column_list.append({"id" : "range_type", "label" : "Minutes", "type" : "string"})
+    elif range_type == "hours":
+        range_type_label = "Hours"
+        column_list.append({"id" : "range_type", "label" : "Hours", "type" : "string"})    
+    elif range_type == "days":
+        range_type_label = "Days"
+        column_list.append({"id" : "range_type", "label" : "Days", "type" : "string"})    
+    elif range_type == "weeks":
+        range_type_label = "Weeks"
+        column_list.append({"id" : "range_type", "label" : "Weeks", "type" : "string"})    
+    elif range_type == "months":
+        range_type_label = "Months"
+        column_list.append({"id" : "range_type", "label" : "Months", "type" : "string"})    
+    elif range_type == "Years":
+        range_type_label = "Years"
+        column_list.append({"id" : "range_type", "label" : "Years", "type" : "string"})    
+    else:
+        dict["error"] = "Valid range_type not specfied."
+        dict["status"] = 500
+        return HttpResponse(json.dumps(dict))
+
+    column_list.append({"id" : "twitter", "label" : "Twitter", "type" : "number"})
+    column_list.append({"id" : "facebook", "label" : "Facebook", "type" : "number"})
+    column_list.append({"id" : "total", "label" : "Total", "type" : "number"})
+    data["cols"] = column_list
+    
+    bucket_dict = {}
+    now = datetime.now()
+    for val in range(0, range_duration):
+        dt = now - timedelta(days=range_duration - val - 1)
+        m = "0" + str(dt.month)
+        d = "0" + str(dt.day)
+        key = str(dt.year) + m[-2:] + d[-2:]
+        print "KEY", key
+        bucket_dict[key] = {"twitter" : 0, "facebook" : 0, "datetime": dt}
+
+    for share in shares:
+        dt = share.created_at
+        m = "0" + str(dt.month)
+        d = "0" + str(dt.day)
+        key = str(dt.year) + m[-2:] + d[-2:]
+        print "key", key
+        if key in bucket_dict:
+            print "in bucket"
+            if share.from_account_type == "F":
+                bucket_dict[key]["facebook"] =  bucket_dict[key]["facebook"] + share.reach()
+            elif share.from_account_type == "T":
+                bucket_dict[key]["twitter"] =  bucket_dict[key]["twitter"] + share.reach()
+
+    print "bucket list", bucket_dict
+    rows_list = []
+    for key in sorted(bucket_dict.keys()):
+        print "KEY", key
+        rows_list.append({"c" : [{"v" : str(key)[4:6] + "/" + str(key)[6:]},
+                                 {"v" : bucket_dict[key]["twitter"]},
+                                 {"v" : bucket_dict[key]["facebook"]},
+                                 {"v" : bucket_dict[key]["twitter"] + bucket_dict[key]["facebook"]}]})
+    data["rows"] = rows_list
+
+    dict["status"] = 200
+    dict["data"] = data
+    dict["range_type_label"] = range_type_label
+    print "DATE Returning DICT", dict
+    return HttpResponse(json.dumps(dict))
+
+
+def analytics_date_range_shares(req):
     """Return data organized by date range."""
     dict = {}
     if "event" in req.GET:
@@ -131,6 +255,7 @@ def analytics_date_range(req):
     dict["range_type_label"] = range_type_label
     print "DATE Returning DICT", dict
     return HttpResponse(json.dumps(dict))
+
 
 def analytics_sources_pie(req):
     """Return json for DataTable pie Format totals."""
--- analytics/urls.py
+++ analytics/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
                        url(r'^$', 'analytics.views.analytics', name='analytics'),
                        url(r'ajax/analytics_data$', 'analytics.ajax.analytics_data', name='analytics_data'),
                        url(r'ajax/analytics_sources_pie$', 'analytics.ajax.analytics_sources_pie', name='analytics_sources_pie'),
-                       url(r'ajax/analytics_date_range$', 'analytics.ajax.analytics_date_range', name='analytics_date_range')
+                       url(r'ajax/analytics_date_range_shares$', 'analytics.ajax.analytics_date_range_shares', name='analytics_date_range_shares'),
+                       url(r'ajax/analytics_date_range_reach$', 'analytics.ajax.analytics_date_range_reach', name='analytics_date_range_reach')
 )
 
