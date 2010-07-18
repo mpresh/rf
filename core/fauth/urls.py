72231a31e1cf230e5afa37841f1ea3c078e2bbeb
Mike Preshman <mpresh@mike-preshmans-macbook-pro.local>
2010-07-18 15:04:21 -0500

    Fixing url issue.

--- core/fauth/urls.py
+++ core/fauth/urls.py
@@ -3,10 +3,10 @@ from django.conf import settings
 
 urlpatterns = patterns('',
                        #url(r'facebook_callback/$', 'facebook_auth.facebook_server_callback', name='facebook_callback'),
-                       url(r'facebook_callback/$', 'facebook_auth.facebook_callback', name='facebook_callback'),
-                       url(r'facebook_logout_callback/$', 'facebook_auth.facebook_logout_callback', name='facebook_logout_callback'),
-                       url(r'ajax/facebook_update_feed/$', 'ajax.update_feed', name='facebook_update_feed'),
-                       url(r'ajax/event_facebook_update/(?P<event_id>\d+)/?$', 'ajax.event_facebook_update', name='event_facebook_update'),
-                       url(r'ajax/facebook_message/$', 'ajax.message', name='facebook_message'),
-                       url(r'ajax/facebook_friends/$', 'ajax.friends', name='facebook_friends'),
+                       url(r'facebook_callback/$', 'fauth.facebook_auth.facebook_callback', name='facebook_callback'),
+                       url(r'facebook_logout_callback/$', 'fauth.facebook_auth.facebook_logout_callback', name='facebook_logout_callback'),
+                       url(r'ajax/facebook_update_feed/$', 'fauth.ajax.update_feed', name='facebook_update_feed'),
+                       url(r'ajax/event_facebook_update/(?P<event_id>\d+)/?$', 'fauth.ajax.event_facebook_update', name='event_facebook_update'),
+                       url(r'ajax/facebook_message/$', 'fauth.ajax.message', name='facebook_message'),
+                       url(r'ajax/facebook_friends/$', 'fauth.ajax.friends', name='facebook_friends'),
                        )
