176a7675be99e4ab7b4e6c80e9d53bd488944160
Mike Preshman <mpresh@gmail.com>
2010-09-24 20:49:40 -0500

    Adding soem stuff.

--- static/javascript/embed.js
+++ static/javascript/embed.js
@@ -1,40 +1,30 @@
 LazyLoad=function(){var f=document,g,b={},e={css:[],js:[]},a;function j(l,k){var m=f.createElement(l),d;for(d in k){if(k.hasOwnProperty(d)){m.setAttribute(d,k[d])}}return m}function h(d){var l=b[d];if(!l){return}var m=l.callback,k=l.urls;k.shift();if(!k.length){if(m){m.call(l.scope||window,l.obj)}b[d]=null;if(e[d].length){i(d)}}}function c(){if(a){return}var k=navigator.userAgent,l=parseFloat,d;a={gecko:0,ie:0,opera:0,webkit:0};d=k.match(/AppleWebKit\/(\S*)/);if(d&&d[1]){a.webkit=l(d[1])}else{d=k.match(/MSIE\s([^;]*)/);if(d&&d[1]){a.ie=l(d[1])}else{if((/Gecko\/(\S*)/).test(k)){a.gecko=1;d=k.match(/rv:([^\s\)]*)/);if(d&&d[1]){a.gecko=l(d[1])}}else{if(d=k.match(/Opera\/(\S*)/)){a.opera=l(d[1])}}}}}function i(r,q,s,m,t){var n,o,l,k,d;c();if(q){q=q.constructor===Array?q:[q];if(r==="css"||a.gecko||a.opera){e[r].push({urls:[].concat(q),callback:s,obj:m,scope:t})}else{for(n=0,o=q.length;n<o;++n){e[r].push({urls:[q[n]],callback:n===o-1?s:null,obj:m,scope:t})}}}if(b[r]||!(k=b[r]=e[r].shift())){return}g=g||f.getElementsByTagName("head")[0];q=k.urls;for(n=0,o=q.length;n<o;++n){d=q[n];if(r==="css"){l=j("link",{href:d,rel:"stylesheet",type:"text/css"})}else{l=j("script",{src:d})}if(a.ie){l.onreadystatechange=function(){var p=this.readyState;if(p==="loaded"||p==="complete"){this.onreadystatechange=null;h(r)}}}else{if(r==="css"&&(a.gecko||a.webkit)){setTimeout(function(){h(r)},50*o)}else{l.onload=l.onerror=function(){h(r)}}}g.appendChild(l)}}return{css:function(l,m,k,d){i("css",l,m,k,d)},js:function(l,m,k,d){i("js",l,m,k,d)}}}();
 
-function fill_widget_iframe() {
-    $("#ripple-embed").css({"height": "400px",
-		"width": "200px",
-		"background-color": "red"
-		});
-    $("#ripple-embed").html("<iframe width='100%' height='100%' src='http://www.yahoo.com'></iframe>");
-}
-
 function fill_widget() {
-    //$("#ripple-embed").load("/camp/badge");
-    $("#ripple-embed").load("http://www.ripplefunction.com/camp/badge", function(){
-   LazyLoad.js(["http://www.ripplefunction.com/site_media/javascript/embed-js.js"], 
-                function(){});
-});
- 
+    $("#ripple-embed").css({"height": "250px",
+		"width": "485px",
+                "background-color": "red"
+		});
+    var campaign_id = $("#ripple-embed").text();
+    var url = "http://www.ripplefunction.com/camp/widget?campaign=" + campaign_id;
+    alert(url);
+    //$("#ripple-embed").load(url, function(){alert("HI" + $("#ripple-embed").html())});
+    //$('#ripple-embed').html("hello world");
+    //$.ajax({url: url,
+    //	    success: function(data) {alert(JSON.stringify(data));}
+    //});
+    $.get(url, function(data){alert(data)})
 }
-
-LazyLoad.css(["http://www.ripplefunction.com/site_media/css/overlayA.css", "http://www.ripplefunction.com/site_media/css/cross-site-badge.css"],
-	     function() {
-
-		 LazyLoad.js(["http://code.jquery.com/jquery-1.4.2.min.js", 
-			      "http://www.ripplefunction.com/site_media/javascript/myjscript.js", 
-			      "http://www.ripplefunction.com/site_media/javascript/jquery.tools.min.js",
-			      "http://www.ripplefunction.com/site_media/javascript/charCount.js",
-			      "http://www.ripplefunction.com/site_media/javascript/jquery.cookie.js",
-			      "http://www.ripplefunction.com/site_media/javascript/jquery.showLoading.js",
-			      "http://www.ripplefunction.com/site_media/javascript/ZeroClipboard.js",
-                              "http://www.ripplefunction.com/site_media/javascript/jquery.xdomainajax.js",
-			      "http://connect.facebook.net/en_US/all.js"
-			      ], function () {
-				 fill_widget();
-			     });
-		 
-	     });
  
 
+
+if (typeof jQuery == 'undefined') {
+	LazyLoad.js("http://code.jquery.com/jquery-1.4.2.min.js", 
+		function () {
+			    fill_widget();
+	  });	
+}else{
+     fill_widget();
+}
 
 
